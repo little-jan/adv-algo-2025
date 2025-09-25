57b6433d4d6ca7e6457d8a9960c29fdaa818ac98
iroha <78338683+little-jan@users.noreply.github.com>
2025-09-25 15:43:40 +0800

    project
    Signed-off-by: iroha <78338683+little-jan@users.noreply.github.com>

--- project/problem_B.py
+++ project/problem_B.py
@@ -1,55 +1,34 @@
-def dfs_postorder(adjlist, visited, root):
-    postorder = []
-    def dfs(current):
-        if not visited[current]:
-            visited[current] = True
-            for neighbor in reversed(adjlist[current]):
-                dfs(neighbor)
-            postorder.append(current)
-    dfs(root)
-    return postorder
+def transpose(edges):
+    n = len(edges)
+    result = []
 
-def tranpose(adjlist):
-    n = len(adjlist)
-    result = [[] for _ in range(n)]
-    for u in range(n):
-        for v in adjlist[u]:
-            if v >= n:
-                result.extend([[] for _ in range(v - n + 1)])
-                n = len(result)
-            result[v].append(u)
+    for u in reversed(range(n)):
+        src, v = edges[u]
+        result.append((v, src))
+
     return result
 
-def kosarajus(adjlist):
-    postorder = []
-    visited = [False for _ in adjlist]
-    for i in range(1, len(adjlist)):
-        if not visited[i]:
-            postorder.extend(dfs_postorder(adjlist, visited, i))
-    adjlist = tranpose(adjlist)
-    visited = [False for _ in adjlist]
-    sccs = []
-    for i in reversed(postorder):
-        if not visited[i]:
-            sccs.append(dfs_postorder(adjlist, visited, i))
-    return sccs
 
 num_vertices, num_edges = map(int, input().split())
-vertices = [int(input()) for _ in range(num_vertices)]
-adjlist = [[] for _ in range(num_vertices + 1)]
-for _ in range(num_edges):
-    src, v = map(int, input().split())
-    adjlist[src].append(v)
 
-sccs = kosarajus(adjlist)
-print(sccs)
+vertices = []
+for i in range(num_vertices):
+    vertices.append(int(input()))
+
+edges = []
+for i in range(num_edges):
+    src, v = map(int, input().split())
+    edges.append((src, v))
 
 friend_costs = [0 for _ in range(num_vertices + 1)]
-visited = []
-for i in sccs:   # i is a cycle
-    if len(i) == 1:
-        current = i
-        weight = vertices[i]
-        visited.append()
+seen = [False for _ in range(num_vertices + 1)]
+visited = [False for _ in range(num_vertices + 1)]
 
-print(friend_costs)
+transposed = transpose(edges)
+
+for edge in transposed:
+    src, v = edge
+    friend_costs[v] = max(friend_costs[src], vertices[src-1])
+
+for val in friend_costs[1:]:
+    print(val)
